20aaf84b7b3668b13e17355981583703ad00e6d4
abma <spring@abma.de>
2012-03-02 22:42:17 +0100

    initial commit

---
+++ modinfo.lua
@@ -0,0 +1,16 @@
+return {
+  name='BA Chicken Defense',
+  description='Defend against attacking Chickens!',
+  shortname='BA',
+  version='V2.33',
+  mutator='Official',
+  game='Total Annihilation',
+  shortGame='TA',
+  modtype=1,
+  depend = {
+    'Balanced Annihilation V7.63',
+  },
+  NTAI = {
+    tdfpath='BA';
+  },
+}
---
+++ tools/compress.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+OUTPUT=ba_chicken_defense.sd7
+
+rm -rf $OUTPUT
+7z a -t7z -m0=lzma -mx=9 -mfb=64 -md=32m -ms=off -x!tools -xr!.svn $OUTPUT .
+
